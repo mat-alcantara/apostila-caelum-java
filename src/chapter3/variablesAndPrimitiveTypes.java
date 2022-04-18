fc7d72905c1d733e6218a58053b02a411f556ce9
mat-alcantara <mateuscastro.al@gmail.com>
2022-04-18 08:13:33 -0300

    add variablesAndPrimitiveTypes

---
+++ src/chapter3/variablesAndPrimitiveTypes.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+class variablesAndPrimitiveTypes {
+    public static void main(String[] args) {
+        int january = 15000;
+        int february = 23000;
+        int march = 17000;
+
+        int soma = january + february + march;
+
+        System.out.println(soma);
+    }
+}
