e0a4900342730d1efd5f82faf6e24660ec91a17c
mat-alcantara <mateuscastro.al@gmail.com>
2022-04-18 07:57:51 -0300

    finish first challenge

---
+++ src/chapter2/modificandoHelloWorld.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+class modificandoHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Uma mensagem diferente");
+        System.out.println("Outra mensagem diferente");
+        System.out.println("Mais uma mensaqgem diferente...\n...Mas agora com duas linhas");
+    }
+}
